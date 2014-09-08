f418cdc0069baf896a6636147df61ebc63a7d172
Dan Kilman <dankilman@gmail.com>
2014-09-08 16:09:49 +0300

    cleanup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='0.1',
+    version='1.1a3',
     author='Cloudify',
+    author_email='cosmo-admin@gigaspaces.com',
     packages=['diamond_agent', 'cloudify_handler'],
-    install_requires=['cloudify-plugins-common==3.0',
-                      'diamond',
-                      'configobj'],
-    tests_require=["nose"],
+    install_requires=['cloudify-plugins-common==3.1a3',
+                      'diamond==3.4.421',
+                      'ConfigObj==5.0.6'],
 )
