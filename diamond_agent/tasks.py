919c143bcf21d0394e687d2bf6bfc29826869a63
Michael Sverdlik <michael@gigaspaces.com>
2014-09-11 01:18:44 +0300

    add handling of custom collectors

--- diamond_agent/tasks.py
+++ diamond_agent/tasks.py
@@ -13,131 +13,140 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
-
 import os
 import sys
 import glob
-from shutil import copytree
+from shutil import copytree, copy
 from tempfile import mkdtemp
 from subprocess import call
 from cloudify.decorators import operation
 from cloudify.utils import get_manager_ip
 from configobj import ConfigObj
 
-# TODO: place log in homedir
-# TODO: paths cannot be unix only
-# TODO: check if kill cannot be signal 9
-
 CONFIG_NAME = 'diamond.conf'
+PID_NAME = 'diamond.pid'
+DEFAULT_COLLECTORS = {
+    'CPUCollector': {},
+    'MemoryCollector': {},
+    'LoadAverageCollector': {},
+    'DiskUsageCollector': {}
+}
 
 
 @operation
-def install(ctx, **kwargs):
-    configure_paths(ctx)
-    ctx.runtime_properties['diamond_handlers'] = \
-        'cloudify_handler.cloudify.CloudifyHandler'
+def install(ctx, diamond_config, **kwargs):
+    paths = get_paths(diamond_config.get('prefix'))
+    ctx.runtime_properties['diamond_config'] = paths['config']
+    host = '.'.join([ctx.node_name, ctx.node_id])
+    # TODO: handlers needs to be customizable as collectors
+    handlers = 'cloudify_handler.cloudify.CloudifyHandler'
+    interval = diamond_config.get('interval', 10)
+    create_config(hostname=host,
+                  path_prefix=ctx.deployment_id,
+                  handlers=handlers,
+                  interval=interval,
+                  paths=paths)
 
-    create_config(ctx)
-    disable_all_collectors(ctx.runtime_properties['diamond_col_conf_path'])
+    copy_content(os.path.join(sys.prefix, 'share', 'diamond', 'collectors'),
+                 paths['collectors'])
+    copy_content(os.path.join(sys.prefix, 'etc', 'diamond', 'collectors'),
+                 paths['collectors_config'])
 
-    config = ctx.properties.get('config', {})
-    collectors = config.get('collectors', [])
-    if not collectors:
-        collectors = ['ExampleCollector']
-
-    # collectors = ['CPUCollector', 'MemoryCollector',
-    #               'LoadAverageCollector', 'DiskUsageCollector']
-    enable_collectors(ctx.runtime_properties['diamond_col_conf_path'],
-                      collectors)
+    disable_all_collectors(paths['collectors_config'])
+    config_collectors(ctx,
+                      diamond_config.get('collectors', {}),
+                      paths['collectors_config'],
+                      paths['collectors'])
 
     config_cloudify_handler(
-        os.path.join(ctx.runtime_properties['diamond_hdl_conf_path'],
-                     'CloudifyHandler.conf'))
+        os.path.join(paths['handlers_config'], 'CloudifyHandler.conf'))
 
-    if config.get('autostart') is True:
-        start(ctx)
-    else:
-        ctx.logger.info('autostart canceled')
+    try:
+        start(paths['config'])
+    except OSError as e:
+        ctx.logger.info('Starting diamond failed: {}'.format(e))
 
 
 @operation
 def uninstall(ctx, **kwargs):
-    pass
-
-
-@operation
-def start(ctx, **kwargs):
-    cmd = 'diamond --configfile {}'\
-        .format(os.path.join(ctx.runtime_properties['diamond_config_path'],
-                             CONFIG_NAME))
+    path = os.path.join(ctx.runtime_properties['diamond_config'], CONFIG_NAME)
     try:
-        call(cmd.split())
-    except OSError:
-        ctx.logger.info('Failed starting Diamond')
+        config = ConfigObj(infile=path, file_error=True)
+        pid = config['server']['pid_file']
+        stop(pid)
+    except (OSError, IOError) as e:
+        ctx.logger.info('Stoppign diamond failed: {}'.format(e))
 
 
-@operation
-def stop(ctx, **kwargs):
-    with open('/tmp/diamond.pid') as f:
+def start(conf_path):
+    cmd = 'diamond --configfile {}'.format(os.path.join(conf_path,
+                                                        CONFIG_NAME))
+    call(cmd.split())
+
+
+def stop(pid_path):
+    with open(pid_path) as f:
         pid = int(f.read())
-
-    try:
-        os.kill(pid, 9)
-    except OSError:
-        ctx.logger.info('Failed stopping Diamond')
+    os.kill(pid, 9)
 
 
-def enable_collector(path, collector):
-    conf_path = os.path.join(path, collector + '.conf')
-    config = ConfigObj(infile=conf_path, file_error=True)
-    config['enabled'] = True
+def config_collectors(ctx, collectors, config_path, collectors_path):
+    if not collectors:
+        collectors = DEFAULT_COLLECTORS
+
+    for name, prop in collectors.items():
+        if 'path' in prop.keys():
+            ctx.download_resource(prop['path'],
+                                  os.path.join(collectors_path, name))
+        prop.update({'enabled': True})
+        config_collector(name, config_path, prop)
+
+
+def config_collector(name, path, properties):
+    full_path = os.path.join(path, name + '.conf')
+    config = ConfigObj(infile=full_path)
+    for key, value in properties.items():
+        config[key] = value
     config.write()
 
 
-def disable_collector(path, collector):
-    conf_path = os.path.join(path, collector + '.conf')
-    config = ConfigObj(infile=conf_path, file_error=True)
+def disable_all_collectors(path):
+    """
+    disables all collectors which configs are located at path
+    """
+    files = glob.glob(os.path.join(path, '*.conf'))
+    for path in files:
+        disable_collector(path)
+
+
+def disable_collector(path):
+    """
+    disables single collector
+    """
+    config = ConfigObj(infile=path, file_error=True)
     config['enabled'] = False
     config.write()
 
 
-def enable_collectors(path, collectors):
-    for collector in collectors:
-        enable_collector(path, collector)
-
-
-def disable_all_collectors(path):
-    files = glob.glob(os.path.join(path, '*.conf'))
-    for f in files:
-        collector = os.path.splitext(os.path.basename(f))[0]
-        disable_collector(path, collector)
-
-
-def configure_paths(ctx):
-    try:
-        prefix = ctx.properties['config']['prefix']
-    except KeyError:
+def get_paths(prefix):
+    """
+    creates folder structure and returns dict with full paths
+    """
+    if prefix is None:
         prefix = mkdtemp(prefix='cloudify-')
-
-    ctx.runtime_properties['diamond_config_path'] = \
-        os.path.join(prefix, 'etc')
-    if not os.path.isdir(ctx.runtime_properties['diamond_config_path']):
-        os.makedirs(ctx.runtime_properties['diamond_config_path'])
-
-    ctx.runtime_properties['diamond_col_conf_path'] = \
-        os.path.join(prefix, 'etc', 'collectors')
-    copytree(os.path.join(sys.prefix, 'etc', 'diamond', 'collectors'),
-             ctx.runtime_properties['diamond_col_conf_path'])
-
-    ctx.runtime_properties['diamond_col_path'] = \
-        os.path.join(prefix, 'collectors')
-    copytree(os.path.join(sys.prefix, 'share', 'diamond', 'collectors'),
-             ctx.runtime_properties['diamond_col_path'])
-
-    ctx.runtime_properties['diamond_hdl_conf_path'] = \
-        os.path.join(prefix, 'etc', 'handlers')
-    if not os.path.isdir(ctx.runtime_properties['diamond_hdl_conf_path']):
-        os.makedirs(ctx.runtime_properties['diamond_hdl_conf_path'])
+    paths = {
+        'config': os.path.join(prefix, 'etc'),
+        'collectors_config': os.path.join(prefix, 'etc', 'collectors'),
+        'collectors': os.path.join(prefix, 'collectors'),
+        'handlers_config': os.path.join(prefix, 'etc', 'handlers'),
+        'handlers': os.path.join(prefix, 'handlers'),
+        'pid': os.path.join(prefix, 'var', 'run'),
+        'log': os.path.join(prefix, 'var', 'log')
+    }
+    for path in paths.values():
+        if not os.path.exists(path):
+            os.makedirs(path)
+    return paths
 
 
 def config_cloudify_handler(config_path):
@@ -156,26 +165,20 @@ def config_cloudify_handler(config_path):
     config.write()
 
 
-def copy_collectors(ctx):
-    pass
-
-
-def create_config(ctx):
+def create_config(hostname, path_prefix, handlers, interval, paths):
     """
-    Create config file and write it into config_path
+    Creates main diamond configuration file
     """
     server_config = {
         'server': {
-            'handlers': ctx.runtime_properties['diamond_handlers'],
+            'handlers': handlers,
             'user': '',
             'group': '',
-            'pid_file': '/tmp/diamond.pid',
-            'collectors_path': ctx.runtime_properties['diamond_col_path'],
-            'collectors_config_path':
-                    ctx.runtime_properties['diamond_col_conf_path'],
-            'handlers_config_path':
-                    ctx.runtime_properties['diamond_hdl_conf_path'],
-            'handlers_path': '/usr/share/diamond/handlers/',
+            'pid_file': os.path.join(paths['pid'], PID_NAME),
+            'collectors_path': paths['collectors'],
+            'collectors_config_path': paths['collectors_config'],
+            'handlers_config_path': paths['handlers_config'],
+            'handlers_path': paths['handlers'],
             'collectors_reload_interval': 3600,
         },
         'handlers': {
@@ -184,9 +187,9 @@ def create_config(ctx):
         },
         'collectors': {
             'default': {
-                'hostname': '.'.join([ctx.node_name, ctx.node_id]),
-                'path_prefix': ctx.deployment_id,
-                'interval': ctx.properties['config']['interval'],
+                'hostname': hostname,
+                'path_prefix': path_prefix,
+                'interval': interval,
             },
         },
         'loggers': {
@@ -204,8 +207,8 @@ def create_config(ctx):
             'class': 'handlers.TimedRotatingFileHandler',
             'level': 'DEBUG',
             'formatter': 'default',
-            # 'args': '({}, {}, 1, 7)'.format('/tmp/diamond.log', 'midnight'),
-            'args': "('/tmp/diamond.log', 'midnight', 1, 7)",
+            'args': "('{}', 'midnight', 1, 7)".format(
+                os.path.join(paths['log'], 'diamond.log')),
         },
         'formatter_default': {
             'format': '[%(asctime)s] [%(threadName)s] %(message)s',
@@ -216,6 +219,14 @@ def create_config(ctx):
                        indent_type='',
                        list_values=False,
                        write_empty_values=True)
-    config.filename = os.path.join(
-        ctx.runtime_properties['diamond_config_path'], CONFIG_NAME)
+    config.filename = os.path.join(paths['config'], CONFIG_NAME)
     config.write()
+
+
+def copy_content(src, dest):
+    for item in os.listdir(src):
+        full_path = os.path.join(src, item)
+        if os.path.isdir(full_path):
+            copytree(full_path, os.path.join(dest, item))
+        else:
+            copy(full_path, dest)
--- diamond_agent/tests/test_with_blueprint.py
+++ diamond_agent/tests/test_with_blueprint.py
@@ -1,18 +1,28 @@
-
 import os
 import unittest
-
+from tempfile import mkdtemp
 from cloudify.workflows import local
 
 
 class TestWithBlueprint(unittest.TestCase):
-
     def setUp(self):
-        os.environ['MANAGEMENT_IP'] = 'localhost'
-        self.env = local.Environment(self._blueprint_path())
+        os.environ['MANAGEMENT_IP'] = '127.0.0.1'
+
+    def tearDown(self):
+        self.env.execute('uninstall', task_retries=0)
 
     def test_with_blueprint(self):
-        self.env.execute('uninstall', task_retries=0)
+        inputs = {
+            'diamond_config': {
+                'prefix': mkdtemp(prefix='cloudify-'),
+            }
+        }
+        self.env = self._create_env(inputs)
+        self.env.execute('install', task_retries=0)
+        self.assertTrue(True)
+
+    def _create_env(self, inputs):
+        return local.Environment(self._blueprint_path(), inputs=inputs)
 
     def _blueprint_path(self):
         return os.path.join(os.path.dirname(__file__),
