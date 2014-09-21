da9f61e94f891b0add85333b70f6f7261d7cea98
Michael Sverdlik <michael@gigaspaces.com>
2014-09-21 15:03:16 +0300

    add py26 to tox

--- diamond_agent/tests/test_with_blueprint.py
+++ diamond_agent/tests/test_with_blueprint.py
@@ -1,7 +1,5 @@
 import os
 import time
-import string
-import random
 import cPickle
 import unittest
 import tempfile
@@ -19,7 +17,7 @@ class TestWithBlueprint(unittest.TestCase):
             self.env.execute('uninstall', task_retries=0)
 
     def test_custom_collectors(self):
-        log_path = os.path.join(tempfile.gettempdir(), id_generator())
+        log_path = tempfile.mktemp()
         inputs = {
             'diamond_config': {
                 'prefix': tempfile.mkdtemp(prefix='cloudify-'),
@@ -43,8 +41,6 @@ class TestWithBlueprint(unittest.TestCase):
                 }
             }
         }
-        print inputs['diamond_config']['prefix']
-        print log_path
         self.env = self._create_env(inputs)
         self.env.execute('install', task_retries=0)
 
@@ -60,6 +56,9 @@ class TestWithBlueprint(unittest.TestCase):
                          metric.host.split('.')[0])
         self.assertEqual(self.env.plan['node_instances'][0]['id'],
                          metric.host.split('.')[1])
+
+    def test_default_collectors(self):
+        pass
 
     def _create_env(self, inputs):
         return local.init_env(self._blueprint_path(), inputs=inputs)
@@ -79,7 +78,3 @@ class TestWithBlueprint(unittest.TestCase):
             except IOError:
                 time.sleep(1)
         self.fail()
-
-
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
