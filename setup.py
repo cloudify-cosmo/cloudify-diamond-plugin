afc99a48aaf5d3e1eed88daa98cea5b34d91d5a0
opencm <limor@gigaspaces.com>
2014-10-06 13:18:11 +0300

    Bump version to 1.1m5

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='1.1a4',
+    version='1.1a5',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['diamond_agent', 'cloudify_handler'],
-    install_requires=['cloudify-plugins-common==3.1a4',
+    install_requires=['cloudify-plugins-common==3.1a5',
                       'diamond==3.4.421',
                       'ConfigObj==5.0.6',
                       'psutil==2.1.1'],
