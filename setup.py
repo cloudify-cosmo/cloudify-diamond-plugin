8b8fd084acbb6c3808a36001628a95f197ccbce7
Michael Sverdlik <michael@gigaspaces.com>
2014-10-12 15:18:04 +0300

    fix requiremnets for tests to pass

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['diamond_agent', 'cloudify_handler'],
-    install_requires=['cloudify-plugins-common==3.1a4',
+    install_requires=['cloudify-plugins-common==3.1a5',
                       'diamond==3.4.421',
                       'ConfigObj==5.0.6',
                       'psutil==2.1.1'],
