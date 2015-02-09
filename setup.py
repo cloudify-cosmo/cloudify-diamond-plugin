fc103557f3a52c164b4a172863b744fabbbee2c9
ran <ran@gigaspaces.com>
2015-02-09 14:23:39 +0200

    modified plugins-common dependency to be equal or greater than

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
     packages=['diamond_agent', 'cloudify_handler'],
-    install_requires=['cloudify-plugins-common==3.2a4',
+    install_requires=['cloudify-plugins-common>=3.2a4',
                       'diamond==3.5.0',
                       'ConfigObj==5.0.6',
                       'psutil==2.1.1'],
