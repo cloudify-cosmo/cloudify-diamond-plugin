bc0121c6b8fbe6370e26b7cee936710a026b3512
opencm <limor@gigaspaces.com>
2015-10-20 18:47:17 +0300

    Bump version to 1.3m7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
     packages=['diamond_agent', 'cloudify_handler'],
-    install_requires=['cloudify-plugins-common>=3.3a7',
+    install_requires=['cloudify-plugins-common>=3.3.0a7',
                       'diamond==3.5.0',
                       'ConfigObj==5.0.6',
                       'psutil==2.1.1'],
