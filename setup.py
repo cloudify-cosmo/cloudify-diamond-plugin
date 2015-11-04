834d933add353be075910a048832cbbd45f1ad98
nir0s <nir36g@gmail.com>
2015-11-04 16:11:54 +0200

    CFY-4000 now getting diamond directly from GitHub instead of PyPI

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     description='Cloudify Diamond monitoring plugin',
     packages=['diamond_agent', 'cloudify_handler'],
     install_requires=['cloudify-plugins-common>=3.3rc1',
-                      'diamond==3.5.0',
                       'ConfigObj==5.0.6',
                       'psutil==2.1.1'],
 )
