1246218a56e5db015f49420248ebeb80c877982c
nirb <nirb@gigaspaces.com>
2015-12-16 12:34:33 +0200

    update version to 1.3.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='1.3',
+    version='1.3.1',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
     packages=['diamond_agent', 'cloudify_handler'],
-    install_requires=['cloudify-plugins-common>=3.3rc1',
+    install_requires=['cloudify-plugins-common>=3.3.1',
                       'diamond==3.5.0',
                       'ConfigObj==5.0.6',
                       'psutil==2.1.1'],
