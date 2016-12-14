56e00839f8708241431bbe48f4c14edf90484584
yarivgiga <yariv@gigaspaces.com>
2016-12-14 14:18:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='1.3.5.dev',
+    version='1.3.5.dev0',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
