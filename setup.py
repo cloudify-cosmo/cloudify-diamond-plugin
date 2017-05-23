8cafffe111f7edef16ea0bbc272c8597ee60cf70
adigrabow <adigrabow@gmail.com>
2017-05-23 20:05:52 +0300

    Cfy 6911 update diamond plugin definition to install false (#38)
    * CFY-6911 added install: False to the plugin.yaml file
    
    * CFY-6911 changed the diamond version to 1.3.6
    
    * CFY-6911 changed source url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='1.3.5',
+    version='1.3.6',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
