800442c464f35fb3f87f118eab6fafca39c30880
opencm <opencm@gigaspaces.com>
2018-05-14 12:04:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='1.3.8',
+    version='1.3.9',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
