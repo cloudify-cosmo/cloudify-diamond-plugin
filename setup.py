7a5c75a66eed4da366d5da741bfd021327492483
idob-cloudify <idob@cloudify.co>
2018-09-22 09:44:12 +0300

    fix-diamond-broker-creds

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-diamond-plugin',
-    version='1.3.14',
+    version='1.3.15',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     description='Cloudify Diamond monitoring plugin',
